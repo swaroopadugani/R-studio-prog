ace97fc87597173fde779c95483cde28d08d98c5
swaroopa dugani <swaroopadugani83@gmail.com>
2018-02-24 15:39:43 -0800

    Ananlysis of daimonds based on caret-value

---
+++ Rscript2(datatypes).R
@@ -0,0 +1,23 @@
+#integer
+x <- 1L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#Character
+a <- "Hello"
+typeof(a)
+b <- "2"
+typeof(b)
+
+#logical
+c <- T
+d <- F
+typeof(c)
+typeof(d)
+
+#complex
+w <- 3+2i
+typeof(w)
