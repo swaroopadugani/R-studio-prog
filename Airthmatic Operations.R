d4bf7aa5cba2d930eb545e60009bf56f7ec69148
swaroopadugani <36803061+swaroopadugani@users.noreply.github.com>
2018-03-01 19:34:00 -0800

    Add files via upload

---
+++ Airthmatic Operations.R
@@ -0,0 +1,31 @@
+#Addition
+a <- 10
+b <- 20
+
+c <- a+b
+
+#just enter the object_name to get the value of object
+c
+
+#Devision
+var1 <- 25
+var2 <- 5
+
+result <- var1 / var2
+
+result
+
+#Square_root
+answer <- sqrt(var1)
+answer
+
+#Multiplication
+value <- var1*var2
+value
+
+#Paste funtion to add two variables
+var3 <- "Hi"
+var4 <- "Abhijit"
+
+message <- paste(var3,var4)
+
