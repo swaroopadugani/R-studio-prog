751ce277b628410cb38110c676b5467381594dd0
swaroopadugani <36803061+swaroopadugani@users.noreply.github.com>
2018-03-05 14:28:45 -0800

    Counter while statement

---
+++ counter-while.R
@@ -0,0 +1,19 @@
+
+
+
+while(F){
+  print("xyz")
+}
+
+counter <- 1
+while(counter <= 12 ){
+  print(counter)
+  counter <- counter+1 
+}
+
+counter <- 1
+while(counter <= 20){
+   counter <- counter+1
+   print(counter)
+   
+}
